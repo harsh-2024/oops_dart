c5d5b9239df23f96302bf6dd88fa7381f7e9cc5a
Harsh <tripathiharsh16@gmail.com>
2022-01-07 21:52:44 +0530

    deposit function implemented

--- bankaccountClass.dart
+++ bankaccountClass.dart
@@ -1,4 +1,20 @@
+// class BankAccount {
+//   num? balance;
+//   void getbalance() => print(balance);
+// }
+
 class BankAccount {
-  num? balance;
-  void getbalance() => print(balance);
+  num? _balance =
+      0; // now this balance is not directly accessible.(using underscore before it.)
+  void setBalance(num amount) {
+    if (amount > 0) {
+      _balance = _balance! +
+          amount; // because balance can be null so here _balance! is used
+
+    } else {
+      throw Exception("Amount to be deposited should be non-negative");
+    }
+  }
+
+  num? getbalance() => _balance;
 }
--- myAccount.dart
+++ myAccount.dart
@@ -1,8 +1,20 @@
+// import 'bankaccountClass.dart';
+
+// BankAccount harshaccount = BankAccount();
+
+// void main() {
+//   harshaccount.balance = 100;    // anyone can access your balance in your account from here.
+//   print(harshaccount.balance);
+// }
+
+// Now implementing Encapsulation so that direct balance cannot by accessible...
+
 import 'bankaccountClass.dart';
 
 BankAccount harshaccount = BankAccount();
 
 void main() {
-  // harshaccount.balance = 100;
-  print(harshaccount.balance);
+  print(harshaccount.getbalance()); // after encapsulating balance field.
+  harshaccount.setBalance(100);
+  print(harshaccount.getbalance());
 }
